954a41c72f4ec8ea7326cccde01d8c6b18b6ae2d
William Bruno <rocha_bruno@hotmail.com>
2013-04-11 21:37:30 -0300

    [SEO] alterada pagina como-comprar para onde-comprar

--- inc/class/Seo.class.php
+++ inc/class/Seo.class.php
@@ -15,7 +15,7 @@ class Seo
 	public static function getURL(){
 		return self::$url;
 	}
-	
+
 	private static function pg( $pg )
 	{
 		switch( $pg )
@@ -26,70 +26,70 @@ class Seo
 				$arr['title'] = 'Como é feito o Strudel de Maçã ?';
 				$arr['canonical'] = self::$url . '/como-e-feito.html';
 				break;
-				
-			case 'class="como-comprar"':
+
+			case 'class="onde-comprar"':
 				$arr['desc'] = 'Compre strudel de maçã, banana e doce de leite. '.self::$desc_default;
 				$arr['key'] = 'comprar strudel maçã, '.self::$key_default;
-				$arr['title'] = 'Como comprar Strudel de Maça em São Paulo ?';
-				$arr['canonical'] = self::$url . '/como-comprar.html';
+				$arr['title'] = 'Onde comprar Strudel de Maça em São Paulo ?';
+				$arr['canonical'] = self::$url . '/onde-comprar.html';
 				break;
-				
+
 			case 'class="strudel-de-maca"':
 				$arr['desc'] = 'Experimente o tradicional strudel de maçã. Peça OnLine seu Strudel de Maçã. Entregas na Região Metropolitana de São Paulo. Ligue (11) 96309-9227';
 				$arr['key'] = 'apfel strudel, '.self::$key_default;
 				$arr['title'] = 'Strudel de Maça em São Paulo';
 				$arr['canonical'] = self::$url . '/strudel-de-maca.html';
 				break;
-				
+
 			case 'class="strudel-de-doce-de-leite"':
 				$arr['desc'] = 'Prove o delicioso strudel de doce de leite. '.self::$desc_default;
 				$arr['key'] = 'strudel de doce de leite, '.self::$key_default;
 				$arr['title'] = 'Strudel de Doce de Leite | '.self::$title_default;
 				$arr['canonical'] = self::$url . '/strudel-de-doce-de-leite.html';
 				break;
-				
+
 			case 'class="strudel-de-banana"':
 				$arr['desc'] = 'O tropical strudel de banana. '.self::$desc_default;
 				$arr['key'] = 'strudel de banana, '.self::$key_default;
 				$arr['title'] = 'Strudel de Banana | '.self::$title_default;
 				$arr['canonical'] = self::$url . '/strudel-de-banana.html';
 				break;
-				
+
 			case 'class="strudel-de-frango-com-catupiry"':
 				$arr['desc'] = 'Strudel de Frango com Catupiry '.self::$desc_default;
 				$arr['key'] = 'strudel de frango com catupiry, '.self::$key_default;
 				$arr['title'] = 'Strudel de Frango com Catupiry | '.self::$title_default;
 				$arr['canonical'] = self::$url . '/strudel-de-frango-com-catupiry.html';
 				break;
-				
+
 			case 'class="strudel-de-palmito"':
 				$arr['desc'] = 'Strudel de Palmito '.self::$desc_default;
 				$arr['key'] = 'strudel de palmito, '.self::$key_default;
 				$arr['title'] = 'Strudel de Palmito | '.self::$title_default;
 				$arr['canonical'] = self::$url . '/strudel-de-palmito.html';
 				break;
-				
+
 			case 'class="strudel-de-bacalhau"':
 				$arr['desc'] = 'Strudel de Bacalhau '.self::$desc_default;
 				$arr['key'] = 'strudel de bacalhau, '.self::$key_default;
 				$arr['title'] = 'Strudel de Bacalhau | '.self::$title_default;
 				$arr['canonical'] = self::$url . '/strudel-de-bacalhau.html';
 				break;
-				
+
 			case 'class="apfelstrudel"':
 				$arr['desc'] = 'Apfelstrudel, o original strudel de maçã. Receita tradicional hungara do apfelstrudel, conhecida nos países da Europa Central.';
 				$arr['key'] = 'apfelstrudel, '.self::$key_default;
 				$arr['title'] = 'Apfelstrudel | '.self::$title_default;
 				$arr['canonical'] = self::$url . '/apfelstrudel.html';
 				break;
-				
+
 			case 'class="cardapio"':
 				$arr['desc'] = 'Cardápio dos Strudels. '.self::$desc_default;
 				$arr['key'] = 'diversos sabores de strudel, '.self::$key_default;
-				$arr['title'] = 'Cardápio dos Strudels | '.self::$title_default;
+				$arr['title'] = 'Preço do Strudel | '.self::$title_default;
 				$arr['canonical'] = self::$url . '/cardapio.html';
 				break;
-				
+
 			case 'class="home"':
 				$arr['desc'] = 'Strudel de Maçã em São Paulo. '.self::$desc_default;
 				$arr['key'] = 'apfel strudel, strudel de maça, '.self::$key_default;
@@ -104,7 +104,7 @@ class Seo
 				$arr['canonical'] = self::$url . '/';
 				break;
 		}
-	
+
 		return $arr;
 	}
 }//Seo
--- javascript/all.js
+++ javascript/all.js
@@ -1,153 +1,159 @@
-(function() {
-    var Lib = {
-        ajax: {
-            xhr: function() {
-                var instance = new XMLHttpRequest();
-                return instance;
-            },
-            getJSON: function(options, callback) {
-                var xhttp = this.xhr();
-                options.url = options.url || location.href;
-                options.data = options.data || null;
-                callback = callback || function() {};
-                options.type = options.type || 'json';
-                var url = options.url;
-
-
-                if (options.type == 'jsonp') {
-                    window.jsonCallback = callback;
-                    var $url = url.replace('callback=?', 'callback=jsonCallback');
-                    var script = document.createElement('script');
-                    script.src = $url;
-                    document.body.appendChild(script);
-                }
-                xhttp.open('GET', options.url, true);
-                xhttp.send(options.data);
-                xhttp.onreadystatechange = function() {
-                    if (xhttp.status == 200 && xhttp.readyState == 4) {
-                        callback(xhttp.responseText);
-                    }
-                };
-            }
-        }
-    };
-    window.Lib = Lib;
-})();
-(function(w, d, undefined) {
+(function () {
 	"use strict";
+	/*global window:false, document:false, XMLHttpRequest:false, location:false*/
+	var Lib = {};
+
+	Lib = {
+		ajax: {
+			xhr: function () {
+				var instance = new XMLHttpRequest();
+				return instance;
+			},
+			getJSON: function (options, callback) {
+				var xhttp = this.xhr();
+				options.url = options.url || location.href;
+				options.data = options.data || null;
+				callback = callback || function () {};
+				options.type = options.type || 'json';
+				var url = options.url;
+
+
+				if (options.type === 'jsonp') {
+					window.jsonCallback = callback;
+					var $url = url.replace('callback=?', 'callback=jsonCallback');
+					var script = document.createElement('script');
+					script.src = $url;
+					document.body.appendChild(script);
+				}
+				xhttp.open('GET', options.url, true);
+				xhttp.send(options.data);
+				xhttp.onreadystatechange = function () {
+					if (xhttp.status === 200 && xhttp.readyState === 4) {
+						callback(xhttp.responseText);
+					}
+				};
+			}
+		}
+	};
+	window.Lib = Lib;
+}());
+(function (w, d, undefined) {
+	"use strict";
+
+	/*global _gaq:false, alert:false, navigator:false*/
 
 	/* mascaras er */
 	var v_obj, v_fun;
-	function getId( el ){
+	function getId(el) {
 		return d.getElementById(el);
-	};
-	function mascara(o,f){
-	    v_obj = o;
-	    v_fun = f;
-	    w.setTimeout(execmascara,1);
-	};
-	function execmascara(){
-	    v_obj.value=v_fun(v_obj.value)
-	};
-	function mcep(v){
-	    v=v.replace(/\D/g,"")                    //Remove tudo o que não é dígito
-	    v=v.replace(/^(\d{5})(\d)/,"$1-$2")         //Esse é tão fácil que não merece explicações
-	    return v
-	};
-	function mtel(v){
-	    v=v.replace(/\D/g,"");             //Remove tudo o que não é dígito
-	    v=v.replace(/^(\d{2})(\d)/g,"($1) $2"); //Coloca parênteses em volta dos dois primeiros dígitos
-	    v=v.replace(/(\d)(\d{4})$/,"$1-$2");    //Coloca hífen entre o quarto e o quinto dígitos
-	    return v;
-	};
-	function error(err){
+	}
+	function execmascara() {
+		v_obj.value = v_fun(v_obj.value);
+	}
+	function mascara(o, f) {
+		v_obj = o;
+		v_fun = f;
+		w.setTimeout(execmascara, 1);
+	}
+	function mcep(v) {
+		v = v.replace(/\D/g, "");
+		v = v.replace(/^(\d{5})(\d)/, "$1-$2");
+		return v;
+	}
+	function mtel(v) {
+		v = v.replace(/\D/g, "");
+		v = v.replace(/^(\d{2})(\d)/g, "($1) $2");
+		v = v.replace(/(\d)(\d{4})$/, "$1-$2");
+		return v;
+	}
+	function error(err) {
 		var $address = getId('address');
 
-	    $address.innerHTML = "Não foi possível obter sua posição.";
-	};
-	function success(position){
-	    var url = 'inc/geo.php?address=' + position.coords.latitude + ',' + position.coords.longitude + '&callback=?',
-	    $cep = getId('cep'),
-	    $address = getId('address');
+		$address.innerHTML = "Não foi possível obter sua posição.";
+	}
+	function success(position) {
+		var url = 'inc/geo.php?address=' + position.coords.latitude + ',' + position.coords.longitude + '&callback=?',
+			$cep = getId('cep'),
+			$address = getId('address');
 
 
-	    $cep.value = 'Procurando..';
-	    
-	    Lib.ajax.getJSON({
-	        url: url,
-	        type: 'jsonp'
-	    }, function(data) {
-	       var data = eval( '('+data+')');
+		$cep.value = 'Procurando..';
 
-	       $address.innerHTML = data[0];
-	       $cep.value = data[1];
-	    });
-	};
+		Lib.ajax.getJSON({
+			url: url,
+			type: 'jsonp'
+		}, function (data) {
+			data = eval('(' + data + ')');
+
+			$address.innerHTML = data[0];
+			$cep.value = data[1];
+		});
+	}
 	function switchTag(e, toTag) {
 		var outerHTML = e.outerHTML;
 		outerHTML = outerHTML.replace(/^<([a-z]+)(.*?)>(.*)<\/\1>$/ig, "<" + toTag + "$2>$3</" + toTag + ">");
 		e.outerHTML = outerHTML;
-	};
+	}
 
 	/* bind eventos */
-	w.onload = function(){
+	w.onload = function () {
 		var $telefone = getId('telefone'),
-		$cep = getId('cep'),
-		$email = getId('email'),
-		$tweet = getId('tweet'),
-		$like = getId('like'),
-		$youtube = getId('youtube'),
-		$gplus = getId('gplus'),
-		$address = getId('address'),
-		$geo = getId('js-geo'),
-		$phone = getId('phone'),
-		$form = getId('form'),
-		$noResponsive = getId('no-responsive');
+			$cep = getId('cep'),
+			$email = getId('email'),
+			$tweet = getId('tweet'),
+			$like = getId('like'),
+			$youtube = getId('youtube'),
+			$gplus = getId('gplus'),
+			$address = getId('address'),
+			$geo = getId('js-geo'),
+			$phone = getId('phone'),
+			$form = getId('form'),
+			$noResponsive = getId('no-responsive');
 
 
 
-		$noResponsive.onclick = function(){
+		$noResponsive.onclick = function () {
 			var vps = d.querySelectorAll("meta[name='viewport']");
 			vps[0].content = 'width=960px,initial-scale=0.3,user-scalable=yes';
-		}
-		
+		};
 
-		$telefone.onkeypress = function(){  
-			mascara( this, mtel );  
+
+		$telefone.onkeypress = function () {
+			mascara(this, mtel);
 		};
-		$cep.onkeypress = function(){
-			mascara( this, mcep );  
+		$cep.onkeypress = function () {
+			mascara(this, mcep);
 		};
-		$cep.onkeyup = function(){  
+		$cep.onkeyup = function () {
 			$address.innerHTML = '';
 		};
-		$tweet.onclick = function(){
+		$tweet.onclick = function () {
 			_gaq.push(['_trackEvent', 'twitter Button', this.href, this.title]);
 		};
-		$like.onclick = function(){
+		$like.onclick = function () {
 			_gaq.push(['_trackEvent', 'facebook Button', this.href, this.title]);
 		};
-		$youtube.onclick = function(){
+		$youtube.onclick = function () {
 			_gaq.push(['_trackEvent', 'youtube Button', this.href, this.title]);
 		};
-		$gplus.onclick = function(){
+		$gplus.onclick = function () {
 			_gaq.push(['_trackEvent', 'google plus Button', this.href, this.title]);
 		};
-		$geo.onclick = function(){
+		$geo.onclick = function () {
 			navigator.geolocation.getCurrentPosition(success, error);
 		};
-		$form.onsubmit = function(){
-			if( $telefone.value==='' ){
+		$form.onsubmit = function () {
+			if ($telefone.value === '') {
 				alert('Digite seu telefone para que eu possa te ligar e confirmar teu pedido!!');
 				$telefone.focus();
 				return false;
 			}
-			if( $cep.value==='' ){
+			if ($cep.value === '') {
 				alert('Informe o cep, para eu conseguir te entregar');
 				$cep.focus();
 				return false;
 			}
-			if( $email.value==='' ){
+			if ($email.value === '') {
 				alert('Me diga seu email =)');
 				$email.focus();
 				return false;
@@ -156,22 +162,22 @@
 
 
 		/* hack abrindo em "full" para mobile */
-		w.scrollTo(0,1);
+		w.scrollTo(0, 1);
 
 		/* transformando span#phone em tag a */
-		$phone.setAttribute('href', getId('phone').getAttribute('data-href') );
+		$phone.setAttribute('href', getId('phone').getAttribute('data-href'));
 		$phone.removeAttribute('data-href');
 		switchTag($phone, 'a');
 
 		/* geolocalizacao do cep */
-	    if(navigator.geolocation){
-	    	$geo.style.display = 'block';
+		if (navigator.geolocation) {
+			$geo.style.display = 'block';
 		}
 	}
 
 
 
-})(window, document);
+}(window, document));
 
 
 
