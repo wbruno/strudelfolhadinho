afc68405b6a6b9aed20bd84aa8d57b12405551b8
wBruno <rocha_bruno@hotmail.com>
2014-02-25 22:49:26 -0300

    seo for keyword apfelstrudel

--- seo.js
+++ seo.js
@@ -1,51 +1,51 @@
 var seodefault = {
-    title: 'Strudel Folhadinho - Fazemos o original ApfelStrudel',
-    description: 'Entregas na Região Metropolitana de São Paulo. Ligue (11) 96309-9227',
+    title: 'Strudel Folhadinho',
+    description: 'Entregas na Região Metropolitana de São Paulo.',
     url: 'http://strudelfolhadinho.com.br'
 }
 exports.seo = {
     index: {
-        title: 'Strudel de Maçã | ' + seodefault.title,
-        description: 'Strudel de Maçã é só pedir com a Strudel Folhadinho. Entregamos o original Apfelstrudel alemão, Strudel de Maçã na cidade de São Paulo.',
+        description: 'Strudel de Maçã é só pedir com a Strudel Folhadinho. Entregamos o original Apfelstrudel alemão, o melhor Strudel de Maçã da cidade de São Paulo.',
+        title: 'Strudel de Maçã, encomende o original ApfelStrudel - '  + seodefault.title,
         canonical: seodefault.url
     },
     como: {
-          description: 'Preparo do original strudel de maçã. Peça online seu Strudel de Maçã, e outros sabores. ' + seodefault.description,
-          title: 'Como é feito o Strudel de Maçã ?',
+          description: 'Veja o preparo do original strudel alemão, a verdadeira receita húngara feita pela Strudel Folhadinho. Compre o strudel com a melhor receita.',
+          title: 'Como é feito o Strudel de Maçã ?' + seodefault.title,
           canonical: seodefault.url + '/como-e-feito.html'
     },
     onde: {
-          description: 'Saiba onde comprar o verdadeiro strudel de maçã alemão em São Paulo. ' + seodefault.description,
-          title: 'Onde comprar Strudel de Maça em São Paulo ?',
+          description: 'Saiba como comprar e o preço do original apfelstrudel de maçã alemão em São Paulo. Aceitamos encomendas. ' + seodefault.description,
+          title: 'Onde comprar Strudel de Maça em São Paulo ?' + seodefault.title,
           canonical: seodefault.url + '/onde-comprar.html'
     },
     maca: {
-          description: 'Veja como comprar strudel de maçã. Encomende online seu Strudel de Maçã - Apfelstrudel. ' + seodefault.description,
-          title: 'Strudel de Maçã em São Paulo',
+          description: 'Veja como comprar strudel de maçã. Encomende online seu Apfelstrudel. ' + seodefault.description,
+          title: 'O original ApfelStrudel, Strudel de Maçã em São Paulo',
           canonical: seodefault.url + '/strudel-de-maca.html'
     },
     leite: {
-          description: 'Prove o delicioso strudel de doce de leite. Receita original alemã. ' + seodefault.description,
+          description: 'Prove o delicioso Strudel de Doce de Leite. Receita original alemã. ' + seodefault.description,
           title: 'Strudel de Doce de Leite | ' + seodefault.title,
           canonical: seodefault.url + '/strudel-de-doce-de-leite.html'
     },
     banana: {
-          description: 'O tropical strudel de banana. Experimente o verdadeiro strudel alemão. ' + seodefault.description,
+          description: 'Encomende o tropical strudel de banana. Todo o sabor da finíssima massa do strudel húngaro, com um recheio tipicamente brasileiro. ',
           title: 'Strudel de Banana | ' + seodefault.title,
           canonical: seodefault.url + '/strudel-de-banana.html'
     },
     frango: {
-          description: 'Saiba onde comprar Strudel de Frango com Catupiry em São Paulo. ' + seodefault.description,
+          description: 'Strudel de Frango com Catupiry em São Paulo. ' + seodefault.description,
           title: 'Strudel de Frango com Catupiry | ' + seodefault.title,
           canonical: seodefault.url + '/strudel-de-frango-com-catupiry.html'
     },
     palmito: {
-          description: 'Saiba onde comprar Strudel de Palmito em São Paulo. ' + seodefault.description,
+          description: 'Strudel de Palmito em São Paulo. ' + seodefault.description,
           title: 'Strudel de Palmito | ' + seodefault.title,
           canonical: seodefault.url + '/strudel-de-palmito.html'
     },
     bacalhau: {
-          description: 'Saiba onde comprar Strudel de Bacalhau em São Paulo. ' + seodefault.description,
+          description: 'Strudel de Bacalhau em São Paulo. ' + seodefault.description,
           title: 'Strudel de Bacalhau | ' + seodefault.title,
           canonical: seodefault.url + '/strudel-de-bacalhau.html'
     },
@@ -61,12 +61,12 @@ exports.seo = {
     },
     apfelstrudel: {
           description: 'Apfelstrudel, o original strudel de maçã alemão. Receita tradicional húngara do apfelstrudel, conhecida nos países da Europa Central.',
-          title: 'Apfelstrudel | ' + seodefault.title,
+          title: 'Original Apfelstrudel de Maçã | ' + seodefault.title,
           canonical: seodefault.url + '/apfelstrudel.html'
     },
     cardapio: {
-          description: 'Compre strudel em são paulo, confira os preços dos doces húngaros ' + seodefault.description,
-          title: 'Preço do Strudel | ' + seodefault.title,
+          description: 'Confira os preços desse doce ou salgado húngaro, chamado strudel. Aceitamos encomendas, fornecemos para buffets, festas e recepções Brasil/Alemanha. Encomende online o seu strudel.',
+          title: 'Preço do Strudel em São Paulo | ' + seodefault.title,
           canonical: seodefault.url + '/cardapio.html'
     }
 
