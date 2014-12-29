9593dc4379a35c0d4695591682f90527a4f302dc
William Bruno <rocha_bruno@hotmail.com>
2014-12-29 12:43:06 -0200

    Added newrelic

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['Strudel Folhadinho - Site'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : 'c65ef1519a8c02bd05067b26c71972af2bbbe085',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'info'
+  }
+};
