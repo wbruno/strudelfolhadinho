4646192bacb5f2fbdc911c20da0f32d1748f04c8
William Bruno <rocha_bruno@hotmail.com>
2015-02-09 10:19:25 -0200

    Mini images

--- products.js
+++ products.js
@@ -2,46 +2,38 @@ exports.products = {
     sweet: {
         maca: {
             name: 'Strudel de Maçã', description: 'Massa Folhada, maçã verde, uvas passas, canela da china, cravo e açúcar.',
-            price: '60,00', url: '/strudel-de-maca.html',
-            badge: 'selo-valor-strudel-60.png'
+            price: '60,00', url: '/strudel-de-maca.html'
         },
         leite: {
             name: 'Strudel de Doce de Leite', description: 'Massa Folhada e doce de leite.',
-            price: '60,00', url: '/strudel-de-doce-de-leite.html',
-            badge: 'selo-valor-strudel-60.png'
+            price: '60,00', url: '/strudel-de-doce-de-leite.html'
         },
         banana: {
             name: 'Strudel de Banana', description: 'Massa Folhada, banana, canela da china, cravo e açúcar.',
-            price: '50,00', url: '/strudel-de-banana.html',
-            badge: 'selo-valor-strudel-50.png'
+            price: '50,00', url: '/strudel-de-banana.html'
         },
         chocolate: {
             name: 'Strudel de Chocolate', description: 'Massa Folhada, chocolate meio amargo, açúcar.',
-            price: '60,00', url: '/strudel-de-chocolate.html',
-            badge: 'selo-valor-strudel-60.png'
+            price: '60,00', url: '/strudel-de-chocolate.html'
         }
         ,
         mini: {
             name: 'Mini Strudel', description: 'Todo o sabor do Strudel de Maçã em tamanho mini, individual',
-            price: '3,00', url: '/mini-strudel.html',
-            badge: 'selo-valor-strudel-3.png'
+            price: '3,50', url: '/mini-strudel.html'
         }
     },
     savory: {
         palmito: {
             name: 'Strudel de Palmito', description: 'Massa Folhada, palmito, molho bechamel.',
-            price: '75,00', url: '/strudel-de-palmito.html',
-            badge: 'selo-valor-strudel-75.png'
+            price: '75,00', url: '/strudel-de-palmito.html'
         },
         frango: {
             name: 'Strudel de Frango com Catupiry', description: 'Massa Folhada, frango desfiado, requeijão da marca Catupiry.',
-            price: '75,00', url: '/strudel-de-frango-com-catupiry.html',
-            badge: 'selo-valor-strudel-75.png'
+            price: '75,00', url: '/strudel-de-frango-com-catupiry.html'
         },
         bacalhau: {
             name: 'Strudel de Bacalhau', description: 'Massa Folhada, bacalhau desfiado, molho bechamel.',
-            price: '115,00', url: '/strudel-de-bacalhau.html',
-            badge: 'selo-valor-strudel-115.png'
+            price: '115,00', url: '/strudel-de-bacalhau.html'
         }
     }
 };
